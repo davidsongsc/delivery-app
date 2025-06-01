fdd61670b9dc54752af057333dd7477d2c00155a
Davidson George Silverio Correa <davidsongsc@gmail.com>
2025-06-01 13:28:21 -0300

    ajuste footer

--- src/templates/webpage/index.tsx
+++ src/templates/webpage/index.tsx
@@ -186,7 +186,7 @@ const Home: NextPage = () => {
         </Content>
 
         <Footer className="text-center text-gray-500 bg-white py-6 mt-12">
-          © {new Date().getFullYear()} MinhaEmpresa. Todos os direitos reservados.
+          © {new Date().getFullYear()} DutoSaas. Todos os direitos reservados.
         </Footer>
       </Layout>
     </>
