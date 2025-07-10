570c4fc80865e704d8a9d8673fc0e1af5713d0cd
Davidson George <53156529+davidsongsc@users.noreply.github.com>
2025-07-10 11:07:05 -0300

    teste f

---
+++ src/app/dashboard/financeira/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+
+
+export default function WalletPage() {
+    return (
+        <div className="h-screen bg-black w-screen">
+           <h1 class="text-white"> Financeira teste </h1>
+        </div>
+    );
+}
